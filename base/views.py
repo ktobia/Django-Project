c610db435c560a7ef5a322d41f0862b25cad121b
ktobia <ktobia@uncc.edu>
2025-02-14 21:30:11 -0500

    2:39:03 - User Logout

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -17,10 +18,21 @@ from .forms import RoomForm
 def loginPage(request):
 
     if request.method == 'POST':
-        email = request.POST.get('username')
-        email = request.POST.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
 
 
     context = {}
